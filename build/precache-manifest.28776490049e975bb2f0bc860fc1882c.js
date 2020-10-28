a0fa64d9028812fd6a7db2a63659e987ebdcac45
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2020-10-28 17:57:21 +0700

    ci: :rocket: build app

---
+++ build/precache-manifest.28776490049e975bb2f0bc860fc1882c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ecc0e85adf5e70ceb0525ecf6d8ba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d22657b2fbe7b85d81b",
+    "url": "/static/css/main.e4b9ef87.chunk.css"
+  },
+  {
+    "revision": "66ce35d2b01fac9a2e45",
+    "url": "/static/js/2.7c81737f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7c81737f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d22657b2fbe7b85d81b",
+    "url": "/static/js/main.ed089575.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "14f1740773bc43337d5d6665823f90b1",
+    "url": "/static/media/express-js-seeklogo.com.14f17407.svg"
+  },
+  {
+    "revision": "58846d405c81eb0c6023eac61d379664",
+    "url": "/static/media/laravel-logo.58846d40.png"
+  },
+  {
+    "revision": "3b2acc79820ba3db2e9422638ec100e3",
+    "url": "/static/media/my-removebg-preview.3b2acc79.png"
+  },
+  {
+    "revision": "2a0f406ec750bc0211c16ac13b98a1f3",
+    "url": "/static/media/quasar-logo.2a0f406e.svg"
+  },
+  {
+    "revision": "2bc32987b9a9efbbeff8e9047f28f235",
+    "url": "/static/media/react-seeklogo.com.2bc32987.svg"
+  },
+  {
+    "revision": "c27f74e48e2b0b63cbbb52088c6cc1e1",
+    "url": "/static/media/vuejs-seeklogo.com.c27f74e4.svg"
+  },
+  {
+    "revision": "ac01305c637621195bf96929ba1b13e3",
+    "url": "/static/media/vuetify-seeklogo.com.ac01305c.svg"
+  }
+]);
