8a4d56260f12ac29e4c8d0fe77b2710029fa5d50
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2020-10-31 00:00:35 +0700

    style: :wastebasket: remove commented code

--- src/components/TheMain/homeSectionCard.tsx
+++ src/components/TheMain/homeSectionCard.tsx
@@ -1,22 +1,9 @@
 import * as React from 'react';
-// import { ButtonProps } from '@/components/Button';
 import C from './classes';
-// import { joinClass } from '@/utils';
-// import * as Icon from '@/components/icons';
 
 interface ActionsProps {
     children?: JSX.Element;
 }
-
-// interface ContentListItemProps {
-//     content: string;
-//     [index: string]: any;
-// }
-
-// interface ContentListProps {
-//     items: ContentListItemProps[]
-//     actions?: ButtonProps[]
-// }
 
 interface homeSectionCardProps {
     title: string;
@@ -49,47 +36,6 @@ export default function HomeSectionCard({
         </div>
     )
 }
-
-// export function ContentList({
-//     items
-// }: ContentListProps): JSX.Element {
-//     return (
-//         <ul className={C.homeSectionCard__content__list}>
-//             {items.map((project, i) => (
-//                 <ContentListItem
-//                     {...project}
-//                     key={`ContentListItem_${i}`}
-//                 />
-//             ))}
-//         </ul>
-//     )
-// }
-
-// export function ContentListItem({
-//     content, ...attrs
-// }: ContentListItemProps): JSX.Element {
-//     const baseClass = C.homeSectionCard__content__list__item__content;
-//     const mergedAttrs = { ...attrs, className: joinClass(attrs.class, baseClass) };
-
-//     return (
-//         <li className={C.homeSectionCard__content__list__item}>
-//             <article {...mergedAttrs}>
-//                 <h3 className={C.homeSectionCard__content__list__item__title}>{content}</h3>
-//                 <p className={C.homeSectionCard__content__list__item__desc}>
-//                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptatem facilis eos voluptate accusamus doloremque dignissimos laboriosam voluptatibus, ullam nesciunt.
-//                 </p>
-//             </article>
-//             <div className={C.homeSectionCard__content__list__item__action}>
-//                 <div className={C.homeSectionCard__content__list__item__action__btn} title="View Code">
-//                     <Icon.Code className={C.homeSectionCard__content__list__item__action__icon}/>
-//                 </div>
-//                 <div className={C.homeSectionCard__content__list__item__action__btn} title="View Demo">
-//                     <Icon.GlobeAlt className={C.homeSectionCard__content__list__item__action__icon}/>
-//                 </div>
-//             </div>
-//         </li>
-//     )
-// }
 
 export function Actions({
     children
