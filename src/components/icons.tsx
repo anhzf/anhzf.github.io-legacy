d45145dcfc3889d5751b7ef1a3592c2f267a06d4
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2020-11-02 05:56:48 +0700

    feat: :bento: add menu icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -109,3 +109,16 @@ export function Whatsapp({
         </svg>
     )
 }
+
+export function Menu({
+    className = defaultClass,
+    title = 'Menu',
+    ...attrs
+}: iconProps): JSX.Element {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className={className} {...attrs}>
+            <title>{title}</title>
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+    )
+}
