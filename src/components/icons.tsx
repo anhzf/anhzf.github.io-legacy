47aea76f82bda91e6609ed8e0088c716284ccf20
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2020-11-01 13:21:17 +0700

    refactor: :recycle: refactor attr icon component

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,127 +1,109 @@
-import React from 'react';
+import * as React from 'react';
 
-export function ExternalLink({ ...attrs }) {
-    const defaultClass = 'h-4';
-    const {
-        className = defaultClass,
-        title = 'Open in new tab',
-        ...restAttrs
-    } = attrs;
+interface iconProps extends React.SVGAttributes<SVGElement> {
+    title?: string;
+}
 
+const defaultClass = 'h-4';
+
+export function ExternalLink({
+    className = defaultClass,
+    title = 'Open in new tab',
+    ...attrs
+}: iconProps): JSX.Element {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className={className} {...restAttrs}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className={className} {...attrs}>
             <title>{title}</title>
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
         </svg>
     );
 }
 
-export function Github({ ...attrs }) {
-    const defaultClass = 'h-4';
-    const {
-        className = defaultClass,
-        title = 'Github',
-        ...restAttrs
-    } = attrs;
-
+export function Github({
+    className = defaultClass,
+    title = 'Github',
+    ...attrs
+}: iconProps): JSX.Element {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" className={className} {...restAttrs}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className={className} {...attrs}>
             <title>{title}</title>
             <path d="M10 0a10 10 0 0 0-3.16 19.49c.5.1.68-.22.68-.48l-.01-1.7c-2.78.6-3.37-1.34-3.37-1.34-.46-1.16-1.11-1.47-1.11-1.47-.9-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.9 1.52 2.34 1.08 2.91.83.1-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.94 0-1.1.39-1.99 1.03-2.69a3.6 3.6 0 0 1 .1-2.64s.84-.27 2.75 1.02a9.58 9.58 0 0 1 5 0c1.91-1.3 2.75-1.02 2.75-1.02.55 1.37.2 2.4.1 2.64.64.7 1.03 1.6 1.03 2.69 0 3.84-2.34 4.68-4.57 4.93.36.31.68.92.68 1.85l-.01 2.75c0 .26.18.58.69.48A10 10 0 0 0 10 0"></path>
         </svg>
     )
 }
 
-export function Code({ ...attrs }) {
-    const defaultClass = 'h-4';
-    const {
-        className = defaultClass,
-        title = 'Code',
-        ...restAttrs
-    } = attrs;
-
+export function Code({
+    className = defaultClass,
+    title = 'Code',
+    ...attrs
+}: iconProps): JSX.Element {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className={className} {...restAttrs}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className={className} {...attrs}>
             <title>{title}</title>
             <path fillRule="evenodd" d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
         </svg>
     )
 }
 
-export function GlobeAlt({ ...attrs }) {
-    const defaultClass = 'h-4';
-    const {
-        className = defaultClass,
-        title = 'Code',
-        ...restAttrs
-    } = attrs;
-
+export function GlobeAlt({
+    className = defaultClass,
+    title = 'Code',
+    ...attrs
+}: iconProps): JSX.Element {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className={className} {...restAttrs}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className={className} {...attrs}>
             <title>{title}</title>
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
         </svg>
     )
 }
 
-export function Instagram({ ...attrs }) {
-    const defaultClass = 'h-4';
-    const {
-        className = defaultClass,
-        title = 'Code',
-        ...restAttrs
-    } = attrs;
-
+export function Instagram({
+    className = defaultClass,
+    title = 'Instagram',
+    ...attrs
+}: iconProps): JSX.Element {
     return (
-        <svg viewBox="0 0 24 24" className={className} {...restAttrs}>
+        <svg viewBox="0 0 24 24" className={className} {...attrs}>
             <title>{title}</title>
             <path fill="currentColor" d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z" />
         </svg>
     )
 }
 
-export function Facebook({ ...attrs }) {
-    const defaultClass = 'h-4';
-    const {
-        className = defaultClass,
-        title = 'Code',
-        ...restAttrs
-    } = attrs;
-
+export function Facebook({
+    className = defaultClass,
+    title = 'Facebook',
+    ...attrs
+}: iconProps): JSX.Element {
     return (
-        <svg viewBox="0 0 24 24" className={className} {...restAttrs}>
+        <svg viewBox="0 0 24 24" className={className} {...attrs}>
             <title>{title}</title>
             <path fill="currentColor" d="M12 2.04C6.5 2.04 2 6.53 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.85C10.44 7.34 11.93 5.96 14.22 5.96C15.31 5.96 16.45 6.15 16.45 6.15V8.62H15.19C13.95 8.62 13.56 9.39 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96A10 10 0 0 0 22 12.06C22 6.53 17.5 2.04 12 2.04Z" />
         </svg>
     )
 }
 
-export function Mail({ ...attrs }) {
-    const defaultClass = 'h-4';
-    const {
-        className = defaultClass,
-        title = 'Code',
-        ...restAttrs
-    } = attrs;
-
+export function Mail({
+    className = defaultClass,
+    title = 'Mail',
+    ...attrs
+}: iconProps): JSX.Element {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className={className} {...restAttrs}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className={className} {...attrs}>
             <title>{title}</title>
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
         </svg>
     )
 }
 
-export function Whatsapp({ ...attrs }) {
-    const defaultClass = 'h-4';
-    const {
-        className = defaultClass,
-        title = 'Code',
-        ...restAttrs
-    } = attrs;
-
+export function Whatsapp({
+    className = defaultClass,
+    title = 'Whatsapp',
+    ...attrs
+}: iconProps): JSX.Element {
     return (
-        <svg viewBox="0 0 24 24" className={className} {...restAttrs}>
+        <svg viewBox="0 0 24 24" className={className} {...attrs}>
             <title>{title}</title>
             <path fill="currentColor" d="M12.04 2C6.58 2 2.13 6.45 2.13 11.91C2.13 13.66 2.59 15.36 3.45 16.86L2.05 22L7.3 20.62C8.75 21.41 10.38 21.83 12.04 21.83C17.5 21.83 21.95 17.38 21.95 11.92C21.95 9.27 20.92 6.78 19.05 4.91C17.18 3.03 14.69 2 12.04 2M12.05 3.67C14.25 3.67 16.31 4.53 17.87 6.09C19.42 7.65 20.28 9.72 20.28 11.92C20.28 16.46 16.58 20.15 12.04 20.15C10.56 20.15 9.11 19.76 7.85 19L7.55 18.83L4.43 19.65L5.26 16.61L5.06 16.29C4.24 15 3.8 13.47 3.8 11.91C3.81 7.37 7.5 3.67 12.05 3.67M8.53 7.33C8.37 7.33 8.1 7.39 7.87 7.64C7.65 7.89 7 8.5 7 9.71C7 10.93 7.89 12.1 8 12.27C8.14 12.44 9.76 14.94 12.25 16C12.84 16.27 13.3 16.42 13.66 16.53C14.25 16.72 14.79 16.69 15.22 16.63C15.7 16.56 16.68 16.03 16.89 15.45C17.1 14.87 17.1 14.38 17.04 14.27C16.97 14.17 16.81 14.11 16.56 14C16.31 13.86 15.09 13.26 14.87 13.18C14.64 13.1 14.5 13.06 14.31 13.3C14.15 13.55 13.67 14.11 13.53 14.27C13.38 14.44 13.24 14.46 13 14.34C12.74 14.21 11.94 13.95 11 13.11C10.26 12.45 9.77 11.64 9.62 11.39C9.5 11.15 9.61 11 9.73 10.89C9.84 10.78 10 10.6 10.1 10.45C10.23 10.31 10.27 10.2 10.35 10.04C10.43 9.87 10.39 9.73 10.33 9.61C10.27 9.5 9.77 8.26 9.56 7.77C9.36 7.29 9.16 7.35 9 7.34C8.86 7.34 8.7 7.33 8.53 7.33Z" />
         </svg>
